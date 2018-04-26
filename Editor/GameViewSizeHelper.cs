c5e87561108ae7a4770fba21f4881801bcb7d335
kyusyukeigo <anchan828@gmail.com>
2018-04-26 17:23:11 +0900

    Update GameViewSizeHelper.cs
    2017.4に対応

--- Editor/GameViewSizeHelper.cs
+++ Editor/GameViewSizeHelper.cs
@@ -20,10 +20,8 @@ namespace Kyusyukeigo.Helper
 
         #region private Fiald
 
-        static string assemblyName = "UnityEditor.dll";
-        static Type gameViewSizeType = Types.GetType("UnityEditor.GameViewSizeType", assemblyName);
-        static Type gameViewSize = Types.GetType("UnityEditor.GameViewSize", assemblyName);
-        static Type gameViewSizes = Types.GetType("UnityEditor.ScriptableSingleton`1", assemblyName).MakeGenericType(Types.GetType("UnityEditor.GameViewSizes", assemblyName));
+
+
         static BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.Instance;
 
         #endregion private Fiald
@@ -46,11 +44,11 @@ namespace Kyusyukeigo.Helper
         public static void AddCustomSize(GameViewSizeGroupType groupType, GameViewSize gameViewSize)
         {
             _gameViewSize = gameViewSize;
-            object sizeType = Enum.Parse(gameViewSizeType, gameViewSize.type.ToString());
-		
-            ConstructorInfo ctor = GameViewSizeHelper.gameViewSize.GetConstructor(new Type[]
+            object sizeType = Enum.Parse(Types.gameViewSizeType, gameViewSize.type.ToString());
+
+            ConstructorInfo ctor = Types.gameViewSize.GetConstructor(new Type[]
                 {
-                    gameViewSizeType,
+                    Types.gameViewSizeType,
                     typeof(int),
                     typeof(int),
                     typeof(string)
@@ -63,23 +61,23 @@ namespace Kyusyukeigo.Helper
                     gameViewSize.height,
                     gameViewSize.baseText
                 });
-		
+
             object instance_gameViewSizeGroup = GetGroup(groupType, instance);
-		
+
             if (!Contains(instance_gameViewSizeGroup))
             {
                 AddCustomSize(instance_gameViewSizeGroup, instance_gameViewSize);
             }
         }
-		
+
         public static void AddCustomSize(GameViewSizeGroupType groupType, GameViewSizeType type, int width, int height, string baseText)
         {
-            AddCustomSize(groupType, new GameViewSize{ type = type, width = width, height = height, baseText = baseText });
+            AddCustomSize(groupType, new GameViewSize { type = type, width = width, height = height, baseText = baseText });
         }
-	
+
         public static bool RemoveCustomSize(GameViewSizeGroupType groupType, GameViewSizeType type, int width, int height, string baseText)
         {
-            _gameViewSize = new GameViewSize{ type = type, width = width, height = height, baseText = baseText };
+            _gameViewSize = new GameViewSize { type = type, width = width, height = height, baseText = baseText };
             return Remove(GetGroup(groupType, instance));
         }
         public static bool RemoveCustomSize(GameViewSizeGroupType groupType, GameViewSize gameViewSize)
@@ -89,7 +87,7 @@ namespace Kyusyukeigo.Helper
         }
         public static bool Contains(GameViewSizeGroupType groupType, GameViewSizeType type, int width, int height, string baseText)
         {
-            _gameViewSize = new GameViewSize{ type = type, width = width, height = height, baseText = baseText };
+            _gameViewSize = new GameViewSize { type = type, width = width, height = height, baseText = baseText };
             return Contains(GetGroup(groupType, instance));
         }
         public static bool Contains(GameViewSizeGroupType groupType, GameViewSize gameViewSize)
@@ -101,13 +99,13 @@ namespace Kyusyukeigo.Helper
 
         public static void ChangeGameViewSize(GameViewSizeGroupType groupType, GameViewSizeType type, int width, int height, string baseText)
         {
-            ChangeGameViewSize(groupType, new GameViewSize{ type = type, width = width, height = height, baseText = baseText });
+            ChangeGameViewSize(groupType, new GameViewSize { type = type, width = width, height = height, baseText = baseText });
         }
 
         public static void ChangeGameViewSize(GameViewSizeGroupType groupType, GameViewSize gameViewSize)
         {
-            EditorWindow gameView = EditorWindow.GetWindow(Types.GetType("UnityEditor.GameView", assemblyName));
-            PropertyInfo currentSizeGroupType = Types.GetType("UnityEditor.GameView", assemblyName).GetProperty("currentSizeGroupType", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static);
+            EditorWindow gameView = EditorWindow.GetWindow(Types.gameView);
+            PropertyInfo currentSizeGroupType = Types.gameView.GetProperty("currentSizeGroupType", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static);
             GameViewSizeGroupType currentType = (GameViewSizeGroupType)currentSizeGroupType.GetValue(gameView, null);
             if (groupType != currentType)
             {
@@ -129,7 +127,7 @@ namespace Kyusyukeigo.Helper
             }
             if (index != -1)
             {
-                PropertyInfo selectedSizeIndex = Types.GetType("UnityEditor.GameView", assemblyName).GetProperty("selectedSizeIndex", BindingFlags.Instance | BindingFlags.NonPublic);
+                PropertyInfo selectedSizeIndex = Types.gameView.GetProperty("selectedSizeIndex", BindingFlags.Instance | BindingFlags.NonPublic);
                 selectedSizeIndex.SetValue(gameView, index, null);
             }
         }
@@ -153,7 +151,7 @@ namespace Kyusyukeigo.Helper
             }
             return false;
         }
-		
+
         static bool Contains(object instance_gameViewSizeGroup)
         {
             int gameViewSizeLength = GetCustomCount(instance_gameViewSizeGroup);
@@ -167,14 +165,14 @@ namespace Kyusyukeigo.Helper
             }
             return false;
         }
-	
+
         private static bool GameViewSize_Equals(GameViewSize a, object b)
         {
             int b_width = (int)GetGameSizeProperty(b, "width");
             int b_height = (int)GetGameSizeProperty(b, "height");
             string b_baseText = (string)GetGameSizeProperty(b, "baseText");
             GameViewSizeType b_sizeType = (GameViewSizeType)Enum.Parse(typeof(GameViewSizeType), GetGameSizeProperty(b, "sizeType").ToString());
-		
+
             return a.type == b_sizeType && a.width == b_width && a.height == b_height && a.baseText == b_baseText;
         }
 
@@ -182,33 +180,33 @@ namespace Kyusyukeigo.Helper
         {
             return instance.GetType().GetProperty(name).GetValue(instance, new object[0]);
         }
-	
+
         static object m_instance;
-	
+
         static object instance
         {
             get
             {
                 if (m_instance == null)
                 {
-                    PropertyInfo propertyInfo_gameViewSizes = gameViewSizes.GetProperty("instance");
+                    PropertyInfo propertyInfo_gameViewSizes = Types.gameViewSizes.GetProperty("instance");
                     m_instance = propertyInfo_gameViewSizes.GetValue(null, new object[0]);
                 }
                 return m_instance;
             }
         }
-	
+
         static object GetGroup(GameViewSizeGroupType groupType, object instance_gameViewSizes)
         {
             Type[] returnTypes = new Type[] { groupType.GetType() };
             object[] parameters = new object[] { groupType };
-            return instance_gameViewSizes.GetType().GetMethod("GetGroup", 
+            return instance_gameViewSizes.GetType().GetMethod("GetGroup",
                 bindingFlags,
                 null,
                 returnTypes,
                 null).Invoke(instance_gameViewSizes, parameters);
         }
-	
+
         static object GetGameViewSize(object instance_gameViewSizeGroup, int i)
         {
             Type[] returnTypes = new Type[] { typeof(int) };
@@ -216,19 +214,19 @@ namespace Kyusyukeigo.Helper
             return instance_gameViewSizeGroup.GetType().GetMethod("GetGameViewSize",
                 bindingFlags,
                 null,
-                returnTypes, 
+                returnTypes,
                 null).Invoke(instance_gameViewSizeGroup, parameters);
         }
-	
+
         static int GetCustomCount(object instance_gameViewSizeGroup)
         {
             return (int)instance_gameViewSizeGroup.GetType().GetMethod("GetCustomCount",
                 bindingFlags,
-                null, 
+                null,
                 new Type[0],
                 null).Invoke(instance_gameViewSizeGroup, new object[0]);
         }
-	
+
         static int GetTotalCount(object instance_gameViewSizeGroup)
         {
             return (int)instance_gameViewSizeGroup.GetType().GetMethod("GetTotalCount",
@@ -237,29 +235,46 @@ namespace Kyusyukeigo.Helper
                 new Type[0],
                 null).Invoke(instance_gameViewSizeGroup, new object[0]);
         }
-	
+
         static void AddCustomSize(object instance_gameViewSizeGroup, object instance_gameViewSize)
         {
-            Type[] returnTypes = new Type[] { gameViewSize };
+            Type[] returnTypes = new Type[] { Types.gameViewSize };
             object[] parameters = new object[] { instance_gameViewSize };
             instance_gameViewSizeGroup.GetType().GetMethod("AddCustomSize",
                 bindingFlags,
-                null, 
+                null,
                 returnTypes,
                 null).Invoke(instance_gameViewSizeGroup, parameters);
         }
-	
+
         static void RemoveCustomSize(object instance_gameViewSizeGroup, int index)
         {
-            Type[] returnTypes = new Type[]{ typeof(int) };
+            Type[] returnTypes = new Type[] { typeof(int) };
             object[] parameters = new object[] { index };
-            instance_gameViewSizeGroup.GetType().GetMethod("RemoveCustomSize", 
-                bindingFlags, 
-                null, 
+            instance_gameViewSizeGroup.GetType().GetMethod("RemoveCustomSize",
+                bindingFlags,
+                null,
                 returnTypes,
                 null).Invoke(instance_gameViewSizeGroup, parameters);
         }
 
         #endregion private Method
+
+        #region types
+
+        private static class Types
+        {
+            private static string assemblyName = "UnityEditor.dll";
+            private static Assembly assembly = Assembly.Load(assemblyName);
+
+            public static Type gameView = assembly.GetType("UnityEditor.GameView");
+
+            public static Type gameViewSizeType = assembly.GetType("UnityEditor.GameViewSizeType");
+            public static Type gameViewSize = assembly.GetType("UnityEditor.GameViewSize");
+            public static Type gameViewSizes = assembly.GetType("UnityEditor.ScriptableSingleton`1").MakeGenericType(assembly.GetType("UnityEditor.GameViewSizes"));
+        }
+
+        #endregion types
+
     }
 }
